a7fc089b6c31a9531ff383c607074c8dc7f810e9
snehalin <55582815+snehalin@users.noreply.github.com>
2020-09-18 16:37:20 +0530

    Add files via upload

--- A.java
+++ A.java
@@ -3,8 +3,14 @@ public class A {
 int a;
 	A()
 	{
+		
 	  System.out.println("A constructor");
 		a=10;
+	}
+	
+	A(int a)
+	{
+		this.a=a;
 	}
 	void display()
 	{
--- B.java
+++ B.java
@@ -6,13 +6,29 @@ public class B extends A//child class
 	int a;
 	B()
 	{
-		//A()
+		//super();//A();
 		System.out.println("B constructor");
 		b=20;
 		a=30;
 	}
+	
+	B(int x)
+	{
+		super(x);
+		//super(x);
+		b=x;
+		a=x;
+		
+	}
+	B(int x,int y,int z)
+	{
+		super(x);//parameterised parent
+		b=y;
+		a=z;
+	}
 	void display()//Method Overriding
 	{
 		System.out.println("b="+b+"a="+a+"A a="+super.a);
+		super.display();//parent
 	}
 }
